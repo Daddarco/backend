77917240e20dd2990600c21a183f377feb4ca1bc
Daddarco <daddarco@gmail.com>
2024-06-09 13:53:44 +0200

    v1.0

--- config/plugins.js
+++ config/plugins.js
@@ -4,10 +4,27 @@ module.exports = () => ({
         config: {
           defaultExcludeAdmin: true, // hide admin:: + strapi:: + webhook + plugin::i18n.locale + plugin::content-releases
           defaultHideUpload: true, // hide plugin::upload.file + plugin::upload.folder
-          defaultExcludeComponents: false, // hide components
+          defaultExcludeComponents: true, // hide components
           defaultLayout: 'dagre', // default layout: ELK,Dagre
           defaultEdgesType: 'step', // default edge type: straight,step,smoothstep,bezier
-          hideMarkers: true, // hide relation marker on edges 
+          hideMarkers: false, // hide relation marker on edges 
         }
       },
+    "entity-relationship-chart": {
+      enabled: true,
+      config: {
+        // By default all contentTypes and components are included.
+        // To exlclude strapi's internal models, use:
+        exclude: [
+          "strapi::core-store",
+          "webhook",
+          "admin::permission",
+          "admin::api-token",
+          "plugin::upload.file",
+          "plugin::i18n.locale",
+          "plugin::users-permissions.permission",
+          "plugin::users-permissions.role",
+        ],
+      },
+    },
 });
